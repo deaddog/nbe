8b64cf38df7bc4e43aa665286d82ea7959bd9f30
deadddog <mikkelslsl@gmail.com>
2024-04-18 23:26:41 +0200

    feat: message send func and controller discovery

--- client/spec.go
+++ client/spec.go
@@ -2,21 +2,40 @@ package client
 
 import (
 	"errors"
+	"fmt"
 	"log/slog"
 	"net"
+	"strconv"
+	"time"
+
+	"github.com/deaddog/nbe/frames"
 )
 
 type Client struct {
-	conn *net.UDPConn
+	logger *slog.Logger
+	conn   *net.UDPConn
+	appId  frames.AppId
+	serial frames.Serial
+	next   frames.MessageId
 }
 
-func New() *Client {
+func New(appId frames.AppId, logger *slog.Logger) (*Client, error) {
+	if err := appId.Validate(); err != nil {
+		return nil, err
+	}
+
 	return &Client{
-		conn: nil,
+		logger: logger.With("nbe-app", appId),
+		conn:   nil,
+		appId:  appId,
+		serial: 0,
+		next:   0,
 	}, nil
 }
 
 func (c *Client) Connect(address string) (final error) {
+	c.logger.Debug("connecting")
+
 	server, err := net.ResolveUDPAddr("udp4", address)
 	if err != nil {
 		return err
@@ -27,6 +46,8 @@ func (c *Client) Connect(address string) (final error) {
 		return err
 	}
 
+	c.logger.Debug("connected")
+
 	defer func() {
 		// If discovery fails, the connection is closed
 		if final != nil {
@@ -34,13 +55,75 @@ func (c *Client) Connect(address string) (final error) {
 		}
 	}()
 
+	payload, err := c.SendSync(frames.FunctionDiscovery, "NBE Discovery")
+	if err != nil {
+		return err
+	}
+
+	if serialStr, ok := payload["Serial"]; ok {
+		serial, err := strconv.Atoi(serialStr)
+		if err != nil {
+			return fmt.Errorf("failed parsing serial '%s' in discovery payload: %w", serialStr, err)
+		}
+		c.serial = frames.Serial(serial)
+	} else {
+		return fmt.Errorf("missing Serial in discovery payload")
+	}
+
+	c.logger.Debug("identified controller", "serial", c.serial)
+
 	return nil
 }
 
 func (c *Client) Close() error {
 	if c.conn != nil {
-		slog.Debug("disconnecting")
+		c.logger.Debug("disconnecting")
 		return c.conn.Close()
 	}
 	return nil
+}
+
+// SendSync sends a message and waits for a response
+func (c *Client) SendSync(function frames.Function, payload string) (frames.ResponsePayload, error) {
+	c.logger.Debug("sending message", "id", c.next, "func", function, "payload", payload)
+
+	req := frames.Request{
+		AppId:          c.appId,
+		Serial:         c.serial,
+		EncryptionMode: frames.EncryptionModeNone,
+		Function:       function,
+		Id:             c.next,
+		Password:       "",
+		Timestamp:      time.Now().Truncate(time.Second),
+		Payload:        payload,
+	}
+
+	c.next = (c.next + 1) % 100
+
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+
+	data := frames.EncodeRequest(req)
+
+	_, err := c.conn.Write(data)
+	if err != nil {
+		return nil, err
+	}
+
+	c.logger.Debug("request sent")
+
+	buffer := make([]byte, 4096)
+	resp, err := frames.DecodeResponse(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Code != frames.ResponseCodeOk {
+		return nil, fmt.Errorf("response code was %v, expected %v (OK)", resp.Code, frames.ResponseCodeOk)
+	}
+
+	c.logger.Debug("response received and decoded")
+
+	return resp.Payload, nil
 }
